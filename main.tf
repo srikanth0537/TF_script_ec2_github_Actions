d025623cc3666e75efc23b7e558d8f3f02b30cf6
srikanth0537 <107805310+srikanth0537@users.noreply.github.com>
2022-11-16 16:23:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  # Configuration options
-  region = "ap-south-1"
-}
-
 resource "aws_instance" "TF_web-server" {
   ami           = var.ami
   instance_type = var.instance_type
